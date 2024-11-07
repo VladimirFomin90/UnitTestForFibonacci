a08421bc119ababbfd41f5b6287322598355dd43
VladimirFomin90 <VladimirFomin90@yandex.ru>
2024-11-07 23:40:05 +0500

    Initial commit

---
+++ FibonacciGeneratorTests/FibonacciGeneratorTests.cs
@@ -0,0 +1,21 @@
+﻿using FibonacciGenerator;
+using NUnit.Framework;
+
+namespace FibonacciGeneratorTests;
+
+[TestFixture]
+public class FibonacciTests
+{
+    [Test]
+    public void Generate_ShallThrowException_IfNIsSmallerThanZero()
+    {
+        Assert.Throws<ArgumentException>(
+            () => Fibonacci.Generate(-1));
+    }
+    [Test]
+    public void Generate_ShallThrowException_IfNIsLargerThan46()
+    {
+        Assert.Throws<ArgumentException>(
+            () => Fibonacci.Generate(47));
+    }
+}
